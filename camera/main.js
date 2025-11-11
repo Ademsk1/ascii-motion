7cac2a1ce0a04511b7ecafa2b5254ad88ec436ce
Ademsk1 <adam.illoul24@gmail.com>
2025-11-11 17:24:33 +0000

    fix: remove zoom

--- camera/main.js
+++ camera/main.js
@@ -4,7 +4,6 @@
 const videoElement = document.getElementById("video")
 const canvas = document.getElementById("canvas")
 const stream = await navigator.mediaDevices.getUserMedia({ video: { width: 1280, height: 720 } })
-document.body.style.zoom = "30%";
 videoElement.srcObject = stream
 
 
